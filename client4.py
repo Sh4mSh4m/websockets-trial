50bd82b80563653cf66451e59675e8c2038ac594
Sh4M <hieu2d@gmail.com>
2020-01-03 23:43:56 +0100

    first draft ok

---
+++ client4.py
@@ -0,0 +1,16 @@
+import asyncio
+import json
+import websockets
+
+async def send_msg():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        message = {
+            "action": "register",
+            "source": "server",
+            "change_id": "2",
+        }
+        await websocket.send(json.dumps(message))
+        print(f"Message sent wesh")
+
+asyncio.get_event_loop().run_until_complete(send_msg())
---
+++ listener3.py
@@ -0,0 +1,24 @@
+import asyncio
+import json
+import websockets
+
+async def listen_msg():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        message = {
+            "action": "register",
+            "source": "client",
+            "change_id": "1",
+        }
+        await websocket.send(json.dumps(message))
+        print(f"listener registered wesh")
+        signal = await websocket.recv()
+        print(signal)
+        message = {
+            "action": "unregister",
+            "change_id": "1",
+        }
+        await websocket.send(json.dumps(message))
+        print('signed off properly')
+
+asyncio.get_event_loop().run_until_complete(listen_msg())
